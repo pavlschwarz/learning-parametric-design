0924ec871f3dd5057224f8f6f0f7c0dbd8755f31
Paul Schwarz <paulfelixschwarz@icloud.com>
2022-05-02 12:48:31 +0200

    Arcs and Background

--- code/task-03/sketch.js
+++ code/task-03/sketch.js
@@ -9,5 +9,24 @@ function setup() {
 }
 
 function draw() {
-  background(100);
+  background('#f78fb7');
+
+  
+  fill(0, 0, 0,)
+  strokeWeight(0)
+  arc(
+    0, 200,
+    300, 400,
+    Math.PI / 180 * -90,
+    Math.PI / 180 * 90,
+    OPEN
+  );
+
+  arc(
+    400, 200,
+    300, 400,
+    Math.PI / 180 * 90,
+    Math.PI / 180 * -90,
+    OPEN
+  );
 }
