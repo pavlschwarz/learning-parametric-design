8737adcaeef19d84233cf4b8f231aaed2c60710e
Paul Schwarz <paulfelixschwarz@icloud.com>
2022-05-03 23:16:05 +0200

    Update sketch.js

--- code/task-03/sketch.js
+++ code/task-03/sketch.js
@@ -1,3 +1,7 @@
+const SketchWdidth = 400;
+const SketchHeight = 400;
+
+
 function preload(){
   // preload assets
 }
@@ -6,27 +10,35 @@ function preload(){
 
 function setup() {
   createCanvas(400, 400);
-}
 
-function draw() {
   background('#f78fb7');
 
   
   fill(0, 0, 0,)
   strokeWeight(0)
   arc(
-    0, 200,
-    300, 400,
+    SketchWdidth - SketchHeight, SketchHeight / 2,
+    SketchHeight * 0.75, SketchWdidth,
     Math.PI / 180 * -90,
     Math.PI / 180 * 90,
     OPEN
   );
 
   arc(
-    400, 200,
-    300, 400,
+    SketchWdidth, SketchHeight / 2,
+    SketchHeight * 0.75, SketchWdidth,
     Math.PI / 180 * 90,
     Math.PI / 180 * -90,
     OPEN
   );
+}
+
+function draw() {
+
+  let DropWidth = SketchWdidth * 0.75
+  let DropHeight = SketchHeight * 0.25
+
+  // for ( DropWidth - SketchWidth * 0.25,let DropX = Math.random() * DropWidth)
+
+
 }
